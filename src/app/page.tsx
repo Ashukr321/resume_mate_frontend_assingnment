a0a3c91dfcaf5c7389d6f7b792b779d4346e6d1f
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-08-02 13:52:04 +0530

    add create resume form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Form from "@/components/Form";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center text-2xl font-bold">Main page</h1>
       <Form/>
     </div>
   );
